56fb26226c8400d46cb474f10f46cf4e36dfb107
Arjunj99 <asj397@nyu.edu>
2019-05-13 21:00:40 -0400

    Gottit

---
+++ Assets/Scripts/BackgroundMusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicScript : MonoBehaviour
+{
+
+    private static BackgroundMusicScript instance = null;
+ 	public static BackgroundMusicScript Instance {
+     get { return instance; }
+ }
+ void Awake() {
+     if (instance != null && instance != this) {
+         Destroy(this.gameObject);
+         return;
+     } else {
+         instance = this;
+     }
+     DontDestroyOnLoad(this.gameObject);
+ }
+
+
+}
+
+//Courtesy of: https://answers.unity.com/questions/11314/audio-or-music-to-continue-playing-between-scene-c.html
---
+++ Assets/Scripts/MatChanger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Attached to: Anything that requires a material change based on an enabled collider
+    Function: Changes the material based on whether the collider is enabled or not
+*/
+public class MatChanger : MonoBehaviour
+{
+
+	public Material deactivatedMaterial;
+	public Material activatedMaterial;
+	Collider objectCollider; 
+	bool last_state = false;
+
+
+    void Start()
+    {
+         
+    	objectCollider = GetComponent<Collider>();
+    	last_state = objectCollider.enabled;
+
+    	if(objectCollider.enabled == true)
+    	{
+    		last_state = objectCollider.enabled;
+    		GetComponent<Renderer>().material = activatedMaterial;
+
+    	}
+
+    	else if(objectCollider.enabled == false)
+    	{
+    		last_state = objectCollider.enabled;
+    		GetComponent<Renderer>().material = deactivatedMaterial;
+    	}
+
+    }
+
+
+    void Update()
+    {
+
+    	if(objectCollider.enabled == true && objectCollider.enabled != last_state)
+    	{
+    		last_state = objectCollider.enabled;
+    		GetComponent<Renderer>().material = activatedMaterial;
+
+    	}
+
+    	else if(objectCollider.enabled == false && objectCollider.enabled != last_state)
+    	{
+    		last_state = objectCollider.enabled;
+    		GetComponent<Renderer>().material = deactivatedMaterial;
+    	}
+
+
+
+    }
+}
